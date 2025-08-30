9b1b43a89a892deb2a1c487b9564c0faf17512c6
Ger Hobbelt <ger@hobbelt.com>
2025-08-30 18:28:30 +0200

    uber-minor tidbit: fix compiler errors in basic C example. (Ah well, even when it's only supposed to serve as a test TEXT INPUT file for magika, we may want to do the decent thing? Anyway, this was caught because our blunt project rig generator tools are a wee bit zealous when it comes to including source files they feel they recognize... ;-S )

--- tests_data/basic/c/code.c
+++ tests_data/basic/c/code.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     char c;
     printf("Enter a character: ");
     scanf("%c", &c);
 
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        printf("%c is a letter");
+        printf("%c is a letter", c);
     else
         printf("%c is not a letter", c);
 
